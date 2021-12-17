59ba94b826ccbe09502057bb0efe1179d1182e8d
vgimonnet <valentin.gimonnet18@gmail.com>
2021-12-17 13:23:20 +0100

    Exo 5

---
+++ exo5.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const url = require('url');
+
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/welcome') {
+    return res.end('Hello World !');
+  } else {
+    return res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => console.log('Le serveur tourne sur http://localhost:3000'));
